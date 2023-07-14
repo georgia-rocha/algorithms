0c67a6e448e324b5829f274db26f3c2809a2d687
Georgia <georgiab543@gmail.com>
2023-07-13 23:02:40 -0300

    feat: requisito 2 - Criptografia de inversões - criando teste

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(4, "hello")
+    with pytest.raises(TypeError):
+        encrypt_message(4, 4)
+    with pytest.raises(TypeError):
+        encrypt_message("hello", "hello")
+    assert encrypt_message("hello", 2) == "oll_eh"
+    assert encrypt_message("hello", 0) == "olleh"
