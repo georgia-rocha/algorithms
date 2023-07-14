b0943faf75a8dff6e1936f550abf3c9fe51a0f64
Georgia <georgiab543@gmail.com>
2023-07-13 22:36:25 -0300

    fix: ajustando função do req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,5 +1,5 @@
 def study_schedule(permanence_period, target_time):
-    valor = 0
+    amount = 0
     for period in permanence_period:
         if (period[0] is None or period[1] is None or target_time is None):
             return None
@@ -7,13 +7,6 @@ def study_schedule(permanence_period, target_time):
                 not isinstance(period[1], int)):
             return None
         if ((period[0]) <= (target_time) <= (period[1])):
-            valor += 1
+            amount += 1
 
-    return valor
-
-
-if __name__ == "__main__":
-    permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-    x = study_schedule(permanence_period, 2)
-    print(x)
-    raise NotImplementedError
+    return amount
