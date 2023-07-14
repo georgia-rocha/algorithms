d5555fbf2bed5b0f04e257b86c83a7bd26a8177f
Georgia <georgiab543@gmail.com>
2023-07-13 22:31:07 -0300

    feat: requisito 1 - Algoritmo de busca - número de estudantes no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,19 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    valor = 0
+    for period in permanence_period:
+        if (period[0] is None or period[1] is None or target_time is None):
+            return None
+        if (not isinstance(period[0], int) or
+                not isinstance(period[1], int)):
+            return None
+        if ((period[0]) <= (target_time) <= (period[1])):
+            valor += 1
+
+    return valor
+
+
+if __name__ == "__main__":
+    permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+    x = study_schedule(permanence_period, 2)
+    print(x)
     raise NotImplementedError
