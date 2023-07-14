ade998410f959ec537a96dfcb8f5adf49372d8e3
Georgia <georgiab543@gmail.com>
2023-07-14 17:14:56 -0300

    feat: requisito 5 - Encontrando números repetidos - Algoritmo de busca

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or not isinstance(nums, list):
+        return False
+
+    double_nums = set()
+
+    for num in nums:
+        if not isinstance(num, int) or num <= 0:
+            return False
+        if num in double_nums:
+            return num
+        double_nums.add(num)
+
+    return False
